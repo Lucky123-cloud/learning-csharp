98264dead89b379b84ffdbde33faa596d9174d8d
Lucky Baraka <lucky.baraka@savannahinformatics.com>
2026-04-18 16:37:50 +0300

    feat: add C# basics (variables, input/output)

--- CSharpCrashCourse/Program.cs
+++ CSharpCrashCourse/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name?");
+        string? userName = Console.ReadLine();
+
+        Console.WriteLine("What is your age?");
+        string? age = Console.ReadLine();
+
+        int userAge = 0;
+
+        if (!string.IsNullOrEmpty(age) && int.TryParse(age, out int parsedInt))
+        {
+            userAge = parsedInt;
+        }
+        else
+        {
+            Console.WriteLine("You entered invalid age, defaulting to 0 ");
+        }
+
+        Console.WriteLine($"Hello {userName}, you are {userAge} years old");
+    }
+}
--- CSharpCrashCourse/obj/Debug/net10.0/CSharpCrashCourse.AssemblyInfo.cs
+++ CSharpCrashCourse/obj/Debug/net10.0/CSharpCrashCourse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCrashCourse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4137d3d6e1de7ac24d652d1b43d18e8aaa27c45a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCrashCourse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCrashCourse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
