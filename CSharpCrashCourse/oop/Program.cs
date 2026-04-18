6d85008b7839b7f8e365d7dc9abfc45eaf80ec89
Lucky Baraka <lucky.baraka@savannahinformatics.com>
2026-04-18 22:58:08 +0300

    feat: added oop intro

---
+++ CSharpCrashCourse/oop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Project
+{
+    static void Main(string[] arg)
+    {
+        Person p1 = new Person
+        {
+            age = 20,
+            name = "Lucky"
+        };
+
+        p1.Introduce();
+    }
+}
+class Person
+{
+    public int age;
+    public string? name;
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello, my name is {name} and I am {age} years old");
+    }
+}
